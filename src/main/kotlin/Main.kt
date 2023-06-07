a936b5a1ef7c75183af1f87f2911e50b41629398
Подковырова Екатерина <yspodkovyr@www.mts.ru>
2023-06-07 16:26:04 +0300

    lesson 1-3

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    var myAge: Int = 22  // 32 bit, (-2.5млрд; +2,5млрд)
+    var myShort: Short = 2 // 16 bit
+    var myLong: Long = 25838383 // 64 bit
+    var myNull: Int? = null // null
+
+    var myString: String = "Hello, world!"
+    var myChar: Char = 'a'
+
+    var myFloat: Float = 2.533f
+    var myDouble:  Double = 2.33333545 // хранит после запятой в 2 раза больше чисел, чем float
+
+    var myBoolean: Boolean = true
+
+}
+
