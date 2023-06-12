8f61f69ba2f6c700a76a000e2f21cf32497fd8e1
podkovyrova <podkovyrova.es@mail.ru>
2023-06-12 11:50:23 +0300

    add lesson8 about func

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,62 +1,32 @@
+
+//fun myFunc(arr: Array<String>, count: Int) {
+//    for (i in arr) {
+//        when (i) {
+//            "Bob" -> println("Access denied $count times")
+//            "Peter" -> println("Welcome, Peter")
+//            else -> println("Who are you?")
+//        }
+//    }
+//}
+
 fun main() {
-    var s = "Hello world"
-    // Первый элемент строки
-    println(s[0])
-    println(s.first())
-    // Последний элемент строки
-    println(s[s.length-1])
-    println(s.last())
+//    val names = arrayOf("John", "Bob", "Peter")
+//    myFunc(names, 3)
+//
+//    if (readLine() == "Get all names") {
+//        myFunc(names, 5)
+//    }
+    var x = 2
+    var y = 3
 
-    // функции startsWith и endsWith
-    var cardNumber = "4377 7236 7334 8839"
-    println(cardNumber.startsWith("4377"))
-    println(cardNumber.endsWith("8839"))
+    val result = add(x, y)
 
-    var a = "Hello"
-    // Свойства заключаются в {}
-    println("The length of $a is ${a.length}")
+    println(result)
+}
 
-    // capitalize(), decapitalize(), toUpperCase(), toLowerCase()
+fun add(x: Int, y: Int): Int {
+    return x + y
+}
 
-//    val name = "John"
-
-//    val newName = name.capitalize()
-//    val newName = name.decapitalize()
-//    val newName = name.toUpperCase()
-//    val newName = name.toLowerCase()
-
-    // Функции is Empty, isBlank
-//    val emptyName = "\n"
-//    val emptyName = "  "
-//    println(emptyName.isBlank())
-//    println(emptyName.isEmpty())
-
-    // Функции, удаляющие пробелы trimStart() и trimEnd() и trim()
-    val name = " John "
-//    val newName = name.trimStart().trimEnd()
-    val newName = name.trim()
-    println(newName)
-
-    // Функции replace() и split()
-    val string = "Today today morning evening today"
-
-    val newString = string.toLowerCase().replace("today", "*****")
-    println(newString)
-
-    val list = "milk,bread,oranges,kiwi"
-    val arr = list.split(",")
-    println(arr)
-
-
-    // Ввод данных пользователем
-    println("Enter your name")
-    val input = readLine()
-    println("Hello, $input")
-
-    if (input?.toLowerCase()?.capitalize() == "John") {
-        println("Access denied")
-    } else {
-        println("Welcome")
-    }
-    }
-
+// Сокращение однострочной функции
+// fun add(x: Int, y: Int): Int = x + y
