4d0b722134a21024f480e46923fb4b74ab5540a3
podkovyrova <podkovyrova.es@mail.ru>
2023-06-14 11:53:36 +0300

    cleaning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,5 @@
-import kotlin.concurrent.fixedRateTimer
-
 fun main() {
-    val user1 = User("34655432", "Lock", 25)
-    val user2 = User("34655432", "Lock", 25)
 
-    println(user1)
-    println(user2)
-
-    println(user1 == user2)
-
-    println(user1.hashCode())
-    println(user2.hashCode())
 
 }
 
