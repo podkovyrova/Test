4ad654438bf071d1f266b8dd9009a55b8c11262d
podkovyrova <podkovyrova.es@mail.ru>
2023-06-19 13:44:58 +0300

    add swap() and print() functions for Array<Int>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,29 @@
 fun main() {
-    val a = "Hello"
-    val b = a.getThirdLetter()
-    String
-    println(b)
+//    val a = "Hello"
+//    val b = a.getThirdLetter()
+//
+//    println(b)
 
+    val arr = arrayOf(1, 2, 3, 4, 5)
+
+    arr.swap(1, 3)
+    arr.print()
 
 }
 
 fun String.getThirdLetter(): Char {
     return this[2]
+}
+
+fun Array<Int>.swap(i1: Int, i2: Int) {
+    val el1 = this[i1]
+    val el2 = this[i2]
+    this[i1] = el2
+    this[i2] = el1
+}
+
+fun Array<Int>.print() {
+    for (i in this) {
+        println(i)
+    }
 }
