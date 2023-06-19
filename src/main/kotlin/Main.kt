24ae65f7f2ec1f1f3110b61cafb8876bd7f065eb
podkovyrova <podkovyrova.es@mail.ru>
2023-06-19 13:14:35 +0300

    написала функцию getThirdLetter(), расширяющую объект String

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,12 @@
-//import java.lang.Exception
-//import kotlin.Exception
-
 fun main() {
-    test()
+    val a = "Hello"
+    val b = a.getThirdLetter()
+    String
+    println(b)
+
 
 }
 
-fun test() {
-    val a = 10
-    val b = 2
-
-    val tel = 7987199872
-
-    try {
-         validate(tel)
-    } catch (e: ValidationException) {
-        println(e.message)
-    } catch (e: CharacterCodingException) {
-        println("$e - исключение")
-    } catch (e: ArrayIndexOutOfBoundsException) {
-        println("$e - исключение")
-    }
-
+fun String.getThirdLetter(): Char {
+    return this[2]
 }
-
-fun validate(tel: Long) {
-    if (tel.toString().length < 11) {
-        throw ValidationException("Ошибка при валидации номера телефона")
-    }
-}
-
-//fun hi() {
-////    throw CharacterCodingException()
-//}
-//
-//fun sendMessage() {
-//    throw ArrayIndexOutOfBoundsException()
-//}
-
