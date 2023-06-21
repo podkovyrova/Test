5b370c6a2329daf1a987da3ac9ea8ce106be3e61
podkovyrova <podkovyrova.es@mail.ru>
2023-06-21 16:27:51 +0300

    ready incapsulated tv program

--- src/main/kotlin/TV.kt
+++ src/main/kotlin/TV.kt
@@ -1,18 +1,18 @@
 class TV {
-    private val channels = arrayOf("BBC", "FOX", "2x2")
+    private var channels = arrayOf("BBC", "FOX", "2x2")
     private var currentChannel = channels[0]
     private var type = "AM"
     private var frequency = 200
 
-    fun getChannels() = channels
-
-    override fun toString(): String {
-        return this.toString()
+    fun getChannels(): Array<String> {
+        return channels
     }
 
-    fun watch(channelNumber: Int) {
-        currentChannel = channels[channelNumber]
+    fun setChannels(newChannels: Array<String>) {
+        channels = newChannels
+    }
 
+    private fun setFrequencyAndType(channelNumber: Int) {
         when (channelNumber) {
             0 -> {
                 type = "AM"
@@ -27,6 +27,12 @@ class TV {
                 frequency = 150
             }
         }
+    }
+
+    fun watch(channelNumber: Int) {
+        currentChannel = channels[channelNumber]
+        setFrequencyAndType(channelNumber)
+
         println("You are watching $currentChannel. Channel type is $type. Channel frequency is $frequency")
     }
 
