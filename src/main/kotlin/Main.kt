39610e80f4a4c761c2d15191d32c67a284523366
podkovyrova <podkovyrova.es@mail.ru>
2023-06-21 17:01:08 +0300

    empty save

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,24 @@
 fun main() {
+    // В сетах данные не могут повторяться
+    val mySet = setOf<String>("apple", "banana", "orange", "apple")
+    println(mySet)
+    mySet.toString()
+    /*
+    [apple, banana, orange]
+     */
 
+    // containts() - проверяет есть ли зачение в сете (true / false)
+    val contains = mySet.contains("banana")
+    println(contains)
+
+    // mutable
+    var mutableSet = mutableSetOf("apple", "banana", "orange", "apple")
+    println(mutableSet)
+
+    // add - добавить новый элемент
+    mutableSet.add("mango")
+    println(mutableSet)
+
+    //
 }
 
