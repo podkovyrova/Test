deb8c7c1dde95879b63177842d48164d9f89e77e
podkovyrova <podkovyrova.es@mail.ru>
2023-06-21 15:26:34 +0300

    add info about maps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,78 @@
-val passportNumbers = listOf("WE934", "FR532", "HH663")
-val persons = listOf("Ivan Ivanov", "Boris Borisov","Petr Petrov", "Elon Musk")
 fun main() {
-    println(getPersonByPassportNumber("HH663"))
+    val passports = mapOf(
+        "EK392" to "Ivan Ivanov",
+        "HH934" to "Petr Petrov",
+        "EM66" to "Elon Musk",
+        "EM66" to "Elon Musk 2", // Последний запишется
+        null to "Max Petrov", // Так делать можно, но не нужно
+        "EN222" to null // Так тоже
+    )
+    val person = passports["EM66"]
+//    println(person) // Elon Musk 2
+//    println(passports[null])
+//    println(passports.values) // Значения
+//    println(passports.keys) // Ключи
+//    println(passports.size) // Размер мапы
+//    println(passports.entries) // Возвращает пары ключ-значение
+//    println(passports.containsKey("EM66")) // true / false
+
+
+    val telephones = mapOf(
+        12345 to 16.32,
+        12245 to 165.52,
+        12545 to 35.32
+    )
+//    println(telephones.get(12245))
+
+    val books = mapOf(
+        "Ivan Ivanov" to listOf("Book 1", "Book 2"),
+        "Elon Musk" to listOf("SpaceX", "DOGE is not skam")
+    )
+
+//    println(books["Elon Musk"])
+
+
+
+    // Muteble - изменяемые мапы
+    val mutablePassports = mutableMapOf(
+        "EK392" to "Ivan Ivanov",
+        "HH934" to "Petr Petrov",
+        "EM66" to "Elon Musk",
+    )
+    // put - добавление пары в мапу
+//    mutablePassports.put("JD883", "Kate Moss")
+    mutablePassports["JD883"] = "Kate Moss"
+    println(mutablePassports)
+    // replace - заменить значение существующего ключа
+    mutablePassports.replace("JD883", "Walter White")
+    println(mutablePassports)
+    // remove - удаляем пару по ключу
+    mutablePassports.remove("JD883")
+    println(mutablePassports)
+    // clear - почистить мапу
+//    mutablePassports.clear()
+//    println(mutablePassports)
+    // putAll - принимает другие мапы
+
+
+    // Работа с циклами в мапе
+    for (i in mutablePassports) {
+        println(i)
+    }
+
+    // EK392=Ivan Ivanov
+    // HH934=Petr Petrov
+    // EM66=Elon Musk
+
+    for (i in mutablePassports) {
+        println(i.key)
+        println(i.value)
+    }
+    // EK392
+    // Ivan Ivanov
+    // HH934
+    // Petr Petrov
+    // EM66
+    // Elon Musk
 }
 
-fun getPersonByPassportNumber(number: String): String {
-    val i = passportNumbers.indexOf(number)
-    val person = persons[i]
-
-    return person
-}
