030f375b6e9d09ecba2c4055fe4a9daeb42f3956
podkovyrova <podkovyrova.es@mail.ru>
2023-06-21 17:31:12 +0300

    add 3 unique functions for set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     // В сетах данные не могут повторяться
-    val mySet = setOf<String>("apple", "banana", "orange", "apple")
+    val mySet = setOf<String>("apple", "banana", "orange", "apple", "kiwi")
     println(mySet)
     mySet.toString()
     /*
@@ -12,13 +12,23 @@ fun main() {
     println(contains)
 
     // mutable
-    var mutableSet = mutableSetOf("apple", "banana", "orange", "apple")
+    var mutableSet = mutableSetOf("apple", "banana", "orange", "apple", "watermelon")
     println(mutableSet)
 
     // add - добавить новый элемент
     mutableSet.add("mango")
     println(mutableSet)
 
-    //
+    // union - объединение двух сетов
+    val unionedSet = mySet.union(mutableSet)
+    println(unionedSet)
+
+    // intersect - вывести общие элементы для двух сетов
+    val intersectedSet = mySet.intersect(mutableSet)
+    println(intersectedSet)
+
+    // substract - вычесть из первого сета второй сет
+    val substractedSet = mySet.subtract(mutableSet)
+    println(substractedSet)
 }
 
