7c69f361e3a34581cc4313093fe0da35a474c4e1
podkovyrova <podkovyrova.es@mail.ru>
2023-06-21 14:11:24 +0300

    bad programm without map

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+val passportNumbers = listOf("WE934", "FR532", "HH663")
+val persons = listOf("Ivan Ivanov", "Boris Borisov","Petr Petrov", "Elon Musk")
 fun main() {
+    println(getPersonByPassportNumber("HH663"))
+}
 
+fun getPersonByPassportNumber(number: String): String {
+    val i = passportNumbers.indexOf(number)
+    val person = persons[i]
+
+    return person
 }
